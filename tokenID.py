f0032dac7668b2c9108b721ccaa2a157ca11b222
Nami <wiolaarvensis@gmail.com>
2021-08-29 22:06:47 +0300

    [NEW] Studies Count

--- tokenID.py
+++ tokenID.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 inpath = 'C:\\Workdir\\DCM_read\\downloads.json'
 
@@ -16,15 +17,34 @@ def list_alljsons(inpath):
 
 
 def main():
-    my_list = list_alljsons(inpath) # сколько всего клиентов, создаем лист он же список
-    a_key: str = "TokenID"
-    values_of_key = [entry[a_key] for entry in my_list if
-                     a_key in entry.keys()]
-    a_set = set(values_of_key) # пишем в переменную сколько клиентов (не уникальных)
-    number_of_unique_values = len(a_set) # сколько там уникальных клиентов, считаем уникальные значения в этом списке
-    print(number_of_unique_values)  # печатаем для проверки сколько этих уникальных клиентов
-    for entry in a_set:             # считаем сколько раз встречается в списке каждый клиент
-        print(f'{entry} : {values_of_key.count(entry)}')
+    database = list_alljsons(inpath)  # сколько всего клиентов, загружаем содержимое файла по пути inpath в переменную
+    # my_list
+    client_name_key: str = 'TokenID'
+    all_client_entries = [entry[client_name_key] for entry in database if
+                          client_name_key in entry.keys()]  # пишем в переменную сколько клиентов (не уникальных)
+    unique_clients = set(all_client_entries)  # пишем в переменную сколько клиентов (уникальных)
+
+    # number_of_unique_values = len(unique_clients)  # сколько там клиентов, считаем значения в этом списке
+
+    # print(number_of_unique_values)  # печатаем для проверки сколько этих уникальных клиентов
+    # for entry in a_set:  # считаем сколько раз встречается в списке каждый клиент
+    #     print(f'{entry} : {values_of_key.count(entry)}')
+
+    client_unique_studies = {}  # пустой словарь для уникальных исследований клиента
+
+    for client in unique_clients:
+        client_studies = [entry['StudyUID'] for entry in database if
+                          'StudyUID' in entry.keys() and entry['TokenID'] == client]  # считаем все исследования
+        # для каждого клиента в базе данных
+        # для каждой записи в базе данных (переменная датабейз)
+        # достать значение по ключу СтадиУИД если СтадиУИД есть среди ключей этой записи
+        # и если по ключу ТокенАйди записан текущий клиент
+
+        client_unique_studies[client] = set(client_studies)  # добавляем запись уникальных исследований
+        # в словарь используя клиента как ключ
+
+    count_of_studies = {key: len(value) for (key, value) in client_unique_studies.items()}
+    pprint(count_of_studies)
 
 
 if __name__ == '__main__':
